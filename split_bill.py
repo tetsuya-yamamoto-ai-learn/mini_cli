d75b0788e1088e922fe0695972d3ca93f2fca6d6
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-09-19 14:41:05 +0900

    divmod()を使ったリファクタリングをした。

--- split_bill.py
+++ split_bill.py
@@ -14,8 +14,9 @@ def main():
     amount = int(input("料金総額を入力してください。"))
 
     # 計算
-    payment = amount // number_of_people  # 一人当たりの値段の計算
-    remainder = amount % number_of_people  # 端数の計算
+    payment, remainder = divmod(amount, number_of_people)
+    # payment = amount // number_of_people  # 一人当たりの値段の計算
+    # remainder = amount % number_of_people  # 端数の計算
 
     # 出力
     print(f"一人当たり{payment}円です。端数は{remainder}円です。")  # 先頭にfを入れると、fstring（f記法）を使用できる。（python3.6から）
